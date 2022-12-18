6ba88f6b0bffae8c0632d096fdc837d0b232e05e
Ezra Maynard <maynard.c.ezra@gmail.com>
2022-12-18 14:46:30 -0600

    Code cleanup, comments, format.

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
 import random
 
 
+#play function, plays one round
 def play():
+    #prompts user for choice
     user = input("What's your choice? 'r' for rock, 'p' for paper, 's' for scissors\n")
     computer = random.choice(['r', 'p', 's'])
 
+    #if user and computer have the same choice it is a tie
     if user == computer:
         return 'tie'
 
-    # r > s, s > p, p > r
+    # rock > scissors, scissors > paper, paper > rock
     if is_win(user, computer):
         return 'You won!'
 
+    # if its not a tie and the user did not win, print you lost
     return 'You lost!'
 
 
@@ -20,6 +24,8 @@ def is_win(player, opponent):
     # r > s, s > p, p > r
     if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
             or (player == 'p' and opponent == 'r'):
+        #return true for a win!
         return True
 
+#calls print function
 print(play())
